66283a631706da787d72097303994e9e4e4d4981
gojko1001 <gojko.novcic2@gmail.com>
2022-04-11 11:41:01 +0200

    [Test] Photo management, user management, app, lists components tested

--- client/src/app/components/admin/photo-management/photo-management.component.spec.ts
+++ client/src/app/components/admin/photo-management/photo-management.component.spec.ts
@@ -1,21 +1,138 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { AdminService } from 'src/app/services/admin.service';
+import { Photo } from 'src/app/model/photo';
 
 import { PhotoManagementComponent } from './photo-management.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-xdescribe('PhotoManagementComponent', () => {
+describe('PhotoManagementComponent', () => {
   let component: PhotoManagementComponent;
   let fixture: ComponentFixture<PhotoManagementComponent>;
+  let adminService: AdminService;
+
+  let photo1: Photo = {
+    id: 1,
+    url: '',
+    username: 'alice',
+    isMain: false,
+    isApproved: false,
+  };
+  let photo2: Photo = {
+    id: 2,
+    url: '',
+    username: 'alice',
+    isMain: false,
+    isApproved: false,
+  };
+  let photo3: Photo = {
+    id: 3,
+    url: '',
+    username: 'bob',
+    isMain: false,
+    isApproved: false,
+  };
+  let photosToApprove: Photo[] = []
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoManagementComponent ]
+      declarations: [ PhotoManagementComponent ],
+      imports: [
+        HttpClientModule
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    photosToApprove = [photo1, photo2, photo3]
     fixture = TestBed.createComponent(PhotoManagementComponent);
     component = fixture.componentInstance;
+    adminService = fixture.debugElement.injector.get(AdminService)
     fixture.detectChanges();
   });
+
+
+  it('should call server to get photos for approval', () => {
+    let spy = spyOn(adminService, 'getPhotosForApproval').and.returnValue(of(photosToApprove))
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should load photos for approval if server successfuly gets them', () => {
+    spyOn(adminService, 'getPhotosForApproval').and.returnValue(of(photosToApprove))
+
+    component.ngOnInit();
+
+    expect(component.photosToApprove.length).toBe(3);
+  })
+
+  it('should render photos and usernames of owners', () => {
+    spyOn(adminService, 'getPhotosForApproval').and.returnValue(of(photosToApprove));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.photosToApprove.forEach(photo => {
+      let photoDiv: HTMLElement = fixture.debugElement.query(By.css('#photo' + photo.id)).nativeElement;
+      expect(photoDiv.innerHTML).toContain(photo.url)
+      expect(photoDiv.innerHTML).toContain(photo.username)
+    })
+  })
+
+  describe('when I approve photo', () => {
+    let photo1ApproveBtn: DebugElement;
+
+    beforeEach(() => {
+      component.photosToApprove = photosToApprove;
+      fixture.detectChanges();
+      photo1ApproveBtn = fixture.debugElement.query(By.css('#photo' + photo1.id)).query(By.css('.btn-success'));
+    })
+    
+    it('should call server to approve photo', () => {
+      let spy = spyOn(adminService, 'approvePhoto').and.returnValue(of({}));
+      
+      photo1ApproveBtn.triggerEventHandler('click', null);
+
+      expect(spy).toHaveBeenCalledWith(photo1.id);
+    })
+    
+    it('should remove approved photo from list', () => {
+      spyOn(adminService, 'approvePhoto').and.returnValue(of({}));
+      
+      photo1ApproveBtn.triggerEventHandler('click', null);
+
+      expect(component.photosToApprove).not.toContain(photo1);
+    })
+  })
+  
+  describe('when I reject photo', () => {
+    let photo2RejectBtn: DebugElement;
+
+    beforeEach(() => {
+      component.photosToApprove = photosToApprove;
+      fixture.detectChanges();
+      photo2RejectBtn = fixture.debugElement.query(By.css('#photo' + photo2.id)).query(By.css('.btn-danger'));
+    })
+    
+    it('should call server to reject photo', () => {
+      let spy = spyOn(adminService, 'rejectPhoto').and.returnValue(of({}));
+      
+      photo2RejectBtn.triggerEventHandler('click', null);
+
+      expect(spy).toHaveBeenCalledWith(photo2.id);
+    })
+    
+    it('should remove approved photo from list', () => {
+      spyOn(adminService, 'rejectPhoto').and.returnValue(of({}));
+      
+      photo2RejectBtn.triggerEventHandler('click', null);
+
+      expect(component.photosToApprove).not.toContain(photo2);
+    })
+  })
 });
--- client/src/app/components/admin/user-management/user-management.component.spec.ts
+++ client/src/app/components/admin/user-management/user-management.component.spec.ts
@@ -1,21 +1,77 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
+import { of } from 'rxjs';
+import { User } from 'src/app/model/user';
+import { AdminService } from 'src/app/services/admin.service';
 
 import { UserManagementComponent } from './user-management.component';
 
-xdescribe('UserManagementComponent', () => {
+describe('UserManagementComponent', () => {
   let component: UserManagementComponent;
   let fixture: ComponentFixture<UserManagementComponent>;
+  let adminService: AdminService;
+  let modalService: BsModalService
+
+  let user1: User = {
+    username: 'alice',
+    roles: ["Member"],
+    gender: '',
+    knownAs: '',
+    profilePhotoUrl: ''
+  }
+
+  let user2: User = {
+    username: 'bob',
+    roles: ["Moderator"],
+    gender: '',
+    knownAs: '',
+    profilePhotoUrl: ''
+  }
+
+  let user3: User = {
+    username: 'john',
+    roles: ["Member", "Moderator"],
+    gender: '',
+    knownAs: '',
+    profilePhotoUrl: ''
+  }
+
+  let testUsers: User[] = []
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserManagementComponent ]
+      declarations: [ UserManagementComponent ],
+      imports: [
+        HttpClientModule,
+        ModalModule.forRoot(),
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    testUsers = [user1, user2, user3];
     fixture = TestBed.createComponent(UserManagementComponent);
     component = fixture.componentInstance;
+    adminService = fixture.debugElement.injector.get(AdminService);
+    modalService = fixture.debugElement.injector.get(BsModalService);
     fixture.detectChanges();
+  });
+
+  it('should call server to load users', () => {
+    let spy = spyOn(adminService, 'getUsersWithRoles').and.returnValue(of(testUsers))
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should load users from server', () => {
+    spyOn(adminService, 'getUsersWithRoles').and.returnValue(of(testUsers))
+
+    component.ngOnInit();
+
+    expect(component.users.length).toBe(3);
   });
 });
